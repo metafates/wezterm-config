0689fd9b8ca9f340acacf2519788ec154fe6c27a
metafates <fates@duck.com>
2023-02-18 16:13:58 +0300

    update

--- font.lua
+++ font.lua
@@ -3,18 +3,18 @@ local wezterm = require 'wezterm'
 local font_size = 18
 local bold = true
 local font_family = ({
-   'Iosevka Nerd Font Mono', -- [1]
-   'JetBrainsMono Nerd Font', -- [2]
-   'FiraCode Nerd Font Mono', -- [3]
-   'Photonico Code', -- [4]
-   'VictorMono Nerd Font', -- [5]
-   'Hurmit Nerd Font Mono', -- [6]
-   'Gintronic', -- [7]
-   'Monocraft', -- [8]
-   'Comic Mono', -- [9]
-   'Input Mono', -- [10]
-   'Comic Code', -- [11]
-})[11]
+       'Iosevka Nerd Font Mono', -- [1]
+       'JetBrainsMono Nerd Font', -- [2]
+       'FiraCode Nerd Font Mono', -- [3]
+       'Photonico Code', -- [4]
+       'VictorMono Nerd Font', -- [5]
+       'Hurmit Nerd Font Mono', -- [6]
+       'Gintronic', -- [7]
+       'Monocraft', -- [8]
+       'Comic Mono', -- [9]
+       'Input Mono', -- [10]
+       'ComicCode Nerd Font Mono', -- [11]
+    })[10]
 
 local options = {}
 if bold then
--- theme.lua
+++ theme.lua
@@ -1,5 +1,7 @@
 local wezterm = require 'wezterm'
 
+local sync_with_system = false
+
 local dark_theme = ({
    'astromouse (terminal.sexy)', -- [1],
    'Catppuccin Frappe', -- [2]
@@ -7,7 +9,11 @@ local dark_theme = ({
    'Rosé Pine (base16)', -- [4]
    'WildCherry', -- [5]
    'nord', -- [6]
-})[2]
+   'Builtin Pastel Dark', -- [7]
+   'Brogrammer (base16)', -- [8]
+   'City Streets (terminal.sexy)', -- [9]
+   'Catppuccin Mocha', -- [10]
+})[10]
 
 local light_theme = ({
    'Catppuccin Latte', -- [1]
@@ -21,15 +27,17 @@ local function scheme_for_appearance(appearance)
    return light_theme
 end
 
-wezterm.on('window-config-reloaded', function(window)
-   local overrides = window:get_config_overrides() or {}
-   local appearance = window:get_appearance()
-   local scheme = scheme_for_appearance(appearance)
-   if overrides.color_scheme ~= scheme then
-      overrides.color_scheme = scheme
-      window:set_config_overrides(overrides)
-   end
-end)
+if sync_with_system then
+   wezterm.on('window-config-reloaded', function(window)
+      local overrides = window:get_config_overrides() or {}
+      local appearance = window:get_appearance()
+      local scheme = scheme_for_appearance(appearance)
+      if overrides.color_scheme ~= scheme then
+         overrides.color_scheme = scheme
+         window:set_config_overrides(overrides)
+      end
+   end)
+end
 
 return {
    color_scheme = dark_theme
